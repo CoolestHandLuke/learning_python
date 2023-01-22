532948495244578b4fd81b0910cb54f938c9a89a
Luke Shea <shealuke@yahoo.com>
2023-01-21 20:53:45 -0600

    Simple MadLib program in Python, playing with input and string literals

---
+++ mad_libs.py
@@ -0,0 +1,55 @@
+
+
+def main():
+    print("Hello, and welcome to MadLibs!")
+    print("Please enter in all the information below to see your hilarious MadLib!")
+    michael_bay()
+
+def michael_bay():
+    name1 = input("Man's name: ")
+    occupation = input("Occupation: ")
+    noun1 = input("Noun: ")
+    noun2 = input("Noun: ")
+    noun3 = input("Noun: ")
+    shape = input("Shape: ")
+    verb1 = input("Verb: ")
+    name2 = input("Woman's name: ")
+    bodyPart = input("Body part: ")
+    verb2 = input("Verb: ")
+    noun4 = input("Noun: ")
+    noun5 = input("Noun: ")
+    rstrnt = input("Restaurant: ")
+    monument = input("Historic Monument: ")
+    verb3 = input("Verb: ")
+    noun6 = input("Noun: ")
+    noun7 = input("Noun: ")
+    noun8 = input("Noun: ")
+    verb4 = input("Verb: ")
+    noun9 = input("Noun: ")
+    adj1 = input("Adjective: ")
+    adj2 = input("Adjective: ")
+    emotion = input("Emotion: ")
+    verb5 = input("Verb: ")
+    noun10 = input("Noun: ")
+    noun11 = input("Noun: ")
+    verb6 = input("Verb: ")
+
+    print(f"{name1} is a normal {occupation}. Then, one day, a {noun1} explodes,")
+    print(f"causing a {noun2} to blow up, and a nearby {noun3} erupts into a {shape} of flames.")
+    print(f"{name1} realizes that he's being chased by the government, who's tyring to {verb1} him.")
+    print(f"While on the run, he teams up with an incredibly attractive woman named {name2}, who")
+    print(f"has an incredible {bodyPart}. She may be from the streets, but she can {verb2} like")
+    print(f"nobody's business. The duo decided to turn the tables on their pursuers by blowing up ")
+    print(f"a {noun4} which triggers a chain reaction, causing the local {noun5}, {rstrnt}, and the") 
+    print(f"{monument} to explode. Then, the bad guy's helicopter gets {verb3} by a piece of {noun6} from") 
+    print(f"when the {noun7} exploded, and the helicopter explodes and falls onto a {noun8}, causing it")
+    print(f"to {verb4} which shoots a fireball straight in to the heart of a {noun9} and detroys the") 
+    print(f"bad guy leader. Everything is {adj1} and the two decided that such a {adj2} ordeal has")
+    print(f"caused them to fall in {emotion} with each other. They decide to celebrate by {verb5}ing")
+    print(f"on the {noun10}, and they even managed to use a {noun11} from the beginning of the movie,")
+    print(f"to {verb6} the whole story together.")
+  
+
+
+if __name__ == "__main__":
+    main()
